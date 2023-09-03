a37f42860fb4e4fdd89842e660beb30c62b7b0b0
tr3sar <josepcdg@gmail.com>
2023-09-04 00:13:20 +0200

    prod env front

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const options = {
 const httpServer = createServer(app);
 const server = https.createServer(options, app);
 const io = new Server(httpServer, {cors: {
-    origin: "https://connectfg.onrender.com"
+    origin: "https://connectfg-902af.web.app"
   }})
 
 app.use(cors({
