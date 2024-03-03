5694783538653fda4588f7ad8a5966b9a653d670
Tr3sar <63011576+Tr3sar@users.noreply.github.com>
2024-03-03 10:22:57 +0100

    Change put to post

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { createNotification, deleteNotification, getNotificationsByUserId } from '../controllers/notification.controller.js'
 
 const notificationRouter = Router()
-notificationRouter.put('/', createNotification)
+notificationRouter.post('/', createNotification)
 notificationRouter.get('/:user_id', getNotificationsByUserId)
 notificationRouter.delete('/:id', deleteNotification)
 
